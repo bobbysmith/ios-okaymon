9c52314eeb4342a5a8fbd7b514f982da072fae4e
gmoreno <gmoreno@us.ibm.com>
2015-11-24 13:28:15 -0600

    Created Pokedex class, moved out of controller

---
+++ Intro-App/Pokedex.swift
@@ -0,0 +1,126 @@
+//
+//  Pokedex.swift
+//  Intro-App
+//
+//  Created by Gaby Moreno on 11/24/15.
+//  Copyright © 2015 Bobby Smith. All rights reserved.
+//
+
+import UIKit
+
+struct Pokemon {
+    var id: Int!
+    var name: String!
+    var image: UIImage!
+    var desc: String!
+}
+
+class Pokedex {
+    
+    var pokedex = [Pokemon]()
+    var currentIndex = 0
+    
+    func getCurrent() -> Int {
+        return self.currentIndex
+    }
+    
+    func setCurrent(newCurrent: Int) -> Void {
+        self.currentIndex = newCurrent
+    }
+    
+    func getCurrentPokemon() -> Pokemon {
+        if self.pokedex.count > self.currentIndex {
+            return self.pokedex[self.currentIndex]
+        } else {
+            return Pokemon()
+        }
+    }
+    
+    func getCurrentName() -> String {
+        if self.pokedex.count > self.currentIndex {
+            if let currentName = self.pokedex[self.currentIndex].name {
+                return currentName.capitalizedString
+            } else {
+                return "No name!"
+            }
+        } else {
+            return "Pokedex not ready yet!"
+        }
+    }
+    
+    func getCurrentImage() -> UIImage {
+        if self.pokedex.count > self.currentIndex {
+            if let currentImage = self.pokedex[self.currentIndex].image {
+                return currentImage
+            } else {
+                return UIImage()
+            }
+        } else {
+            return UIImage()
+        }
+    }
+    
+    func getSize() -> Int {
+        return self.pokedex.count
+    }
+    
+    func loadPokemon(callback: ()->()) {
+        // Create request
+        let url = NSURL(string: "http://okaymon.mybluemix.net/api/pokemon?limit=10")
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {
+            data, response, error in
+            
+            if error != nil
+            {
+                print("error=\(error)")
+                return
+            }
+            
+            let jsonData = data!
+            
+            do {
+                
+                // Try parsing JSON
+                let jsonObj = try NSJSONSerialization.JSONObjectWithData(jsonData, options: NSJSONReadingOptions.AllowFragments)
+                
+                if let dataArray = jsonObj as? NSArray {
+                    
+                    for index in dataArray {
+                        
+                        if let dataDic = index as? NSDictionary {
+                            let pokeID = dataDic["pkdx_id"] as? Int
+                            let pokeName = dataDic["name"] as? String
+                            let pokeDesc = dataDic["description"] as? String
+                            var pokeImage: UIImage!
+                            
+                            if let imageString = dataDic["art_url"] as? String {
+                                if let url = NSURL(string: imageString) {
+                                    if let data = NSData(contentsOfURL: url) {
+                                        pokeImage = UIImage(data: data)
+                                    }
+                                }
+                            }
+                            
+                            let pokemon = Pokemon(id: pokeID, name: pokeName, image: pokeImage, desc: pokeDesc)
+                            
+                            self.pokedex.append(pokemon)
+                            
+                        }
+                    }
+                    
+                    dispatch_async(dispatch_get_main_queue(), {
+                        //self.updateDisplay(self.currentIndex)
+                        callback()
+                    })
+                }
+            }
+            catch let error as NSError {
+                // Catch fires here, with an NSError being thrown from the JSONObjectWithData method
+                print("A JSON parsing error occurred, here are the details:\n \(error)")
+            }        }
+        
+        task.resume()
+    }
+
+}
--- Intro-App/PokedexViewController.swift
+++ Intro-App/PokedexViewController.swift
@@ -12,99 +12,46 @@ class PokedexViewController: UIViewController {
     @IBOutlet weak var PokeNameLabel: UILabel!
     @IBOutlet weak var PokeMainImage: UIImageView!
     
-    struct Pokemon {
-        var name: String!
-        var image: UIImage!
-        var desc: String!
-    }
-    
-    var pokedex = [Pokemon]()
-    var current = 0
+    var pokedex = Pokedex()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        loadPokemon()
-        
+        pokedex.loadPokemon(self.updateDisplay)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    func updateDisplay() {
+        let pokemon = pokedex.getCurrentPokemon()
+        
+        if let currentName = pokemon.name {
+            self.PokeNameLabel.text = "#\(pokemon.id) \(currentName.capitalizedString)"
+        }
+        
+        if let currentImage = pokemon.image {
+            self.PokeMainImage.image = currentImage
+        }
+    }
+    
     @IBAction func showPrevious(sender: AnyObject) {
+        var current = pokedex.getCurrent()
+        
         current = current > 0 ? current-1 : 0
-        updatePokedex(current)
+        
+        pokedex.setCurrent(current)
+        updateDisplay()
     }
     
     @IBAction func showNext(sender: AnyObject) {
-        current = current < self.pokedex.count-1 ? current+1 : current
-        updatePokedex(current)
-    }
-    
-    func updatePokedex(index: Int) {
-        self.PokeNameLabel.text = self.pokedex[self.current].name!.capitalizedString
-        self.PokeMainImage.image = self.pokedex[self.current].image!
-    }
-    
-    func loadPokemon() {
-        // Create request
-        let url = NSURL(string: "http://okaymon.mybluemix.net/api/pokemon?limit=15")
+        var current = pokedex.getCurrent()
         
-        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {
-            data, response, error in
-            
-            if error != nil
-            {
-                print("error=\(error)")
-                return
-            }
-            
-            let jsonData = data!
-            
-            do {
-                
-                // Try parsing JSON
-                let jsonObj = try NSJSONSerialization.JSONObjectWithData(jsonData, options: NSJSONReadingOptions.AllowFragments)
-                
-                if let dataArray = jsonObj as? NSArray {
-                    
-                    for index in dataArray {
-                        
-                        if let dataDic = index as? NSDictionary {
-                            
-                            let pokeName = dataDic["name"] as? String
-                            let pokeDesc = dataDic["description"] as? String
-                            var pokeImage: UIImage!
-                            
-                            if let imageString = dataDic["art_url"] as? String {
-                                if let url = NSURL(string: imageString) {
-                                    if let data = NSData(contentsOfURL: url) {
-                                        pokeImage = UIImage(data: data)
-                                    }
-                                }
-                            }
-                            
-                            let pokemon = Pokemon(name: pokeName, image: pokeImage, desc: pokeDesc)
-                            
-                            self.pokedex.append(pokemon)
-                            
-                        }
-                    }
-                    
-                    dispatch_async(dispatch_get_main_queue(), {
-                        self.updatePokedex(self.current)
-                    })
-                }
-            }
-            catch let error as NSError {
-                // Catch fires here, with an NSError being thrown from the JSONObjectWithData method
-                print("A JSON parsing error occurred, here are the details:\n \(error)")
-            }        }
+        current = current < pokedex.getSize()-1 ? current+1 : current
         
-        task.resume()
+        pokedex.setCurrent(current)
+        updateDisplay()
     }
-    
         
 }
 
